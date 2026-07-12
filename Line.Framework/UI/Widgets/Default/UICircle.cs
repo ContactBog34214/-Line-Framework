7d1631573a8267c67444a93ad684ad7582e06fb5
ContactBog34214 <w2160057868@outlook.com>
2026-07-12 13:38:43 +0800

    feat.添加UICircle

---
+++ Line.Framework/UI/Widgets/Default/UICircle.cs
@@ -0,0 +1,66 @@
+using System.Numerics;
+using Line.Framework.Graphics;
+using Veldrid;
+
+namespace Line.Framework.UI.DefaultWidget;
+
+public class UICircle : UIWidget
+{
+    public RgbaFloat color { get; set; } = new(0, 0, 0, 1);
+    public uint Precision
+    {
+        get;
+        set
+        {
+            if (value >= 3)
+                field = value;
+            else
+                throw new Exception($"Precision cannot be {value}");
+        }
+    } = 20;
+    public Vector2 Middle { get; set; } = new(0.5f);
+
+    public override void RendererContext(RendererContextArgs args)
+    {
+        var cl = args.Collector;
+        var middle = new WindowsRenderer.Vertex(
+            new((float)args.width * Middle.X, (float)args.height * Middle.Y),
+            color,
+            new(new(), new()),
+            null,
+            null,
+            1f
+        );
+        var sz = new Vector2((float)args.width, (float)args.height);
+        for (int i = 0; i < Precision; i++)
+        {
+            var rt = (float)i / Precision * 360f;
+            float cos = (float)Math.Cos(rt * Math.PI / 180f);
+            float sin = (float)Math.Sin(rt * Math.PI / 180f);
+            var v1p = new Vector2(cos + 1f, sin + 1f);
+            v1p /= 2f;
+            v1p *= sz;
+            var v1 = new WindowsRenderer.Vertex(v1p, color, new(new(), new()), null, null, 1f);
+            rt = (float)(i + 1) / Precision * 360f;
+            cos = (float)Math.Cos(rt * Math.PI / 180f);
+            sin = (float)Math.Sin(rt * Math.PI / 180f);
+            var v2p = new Vector2(cos + 1f, sin + 1f);
+            v2p /= 2f;
+            v2p *= sz;
+            var v2 = new WindowsRenderer.Vertex(v2p, color, new(new(), new()), null, null, 1f);
+            cl.DrawVertex([middle, v1, v2], this);
+        }
+    }
+
+    public override bool HitTest(Vector2 mousePixel)
+    {
+        var local = MousePosition(mousePixel); // 相对于控件左上角
+        var size = GetSizeOnScreen();
+        float halfW = size.X / 2f;
+        float halfH = size.Y / 2f;
+        float dx = local.X - halfW;
+        float dy = local.Y - halfH;
+        // 直接使用椭圆不等式
+        return (dx * dx) / (halfW * halfW) + (dy * dy) / (halfH * halfH) <= 1f;
+    }
+}
--- SimpleGame/Program.cs
+++ SimpleGame/Program.cs
@@ -10,7 +10,7 @@ using Line.Framework.UI.DefaultWidget;
 
 namespace SG;
 
-public static unsafe class SimpleGame
+public static class SimpleGame
 {
     static BaseWindow Host;
     static Stopwatch sw = new();
@@ -32,9 +32,11 @@ public static unsafe class SimpleGame
         foreach (var name in names)
             Log.Debug($"Asset:{name}");
 
-        Host = new(Backend: GraphicBackend.Vulkan);
-        Host.Title = "-Line-Framework example";
-        Host.UpdatePerSecond = 10000;
+        Host = new(Backend: GraphicBackend.OpenGL)
+        {
+            Title = "-Line-Framework example",
+            UpdatePerSecond = 10000,
+        };
 
         Host.Resource.Create(
             "Font",
@@ -96,7 +98,7 @@ public static unsafe class SimpleGame
             Parent = Host.Root,
             TextureId = "Cursor",
             Z = 32767,
-            CanTouch=false
+            CanTouch = false,
         };
 
         var title = new UIText(Host.Resource)
@@ -119,26 +121,26 @@ public static unsafe class SimpleGame
             float r = sw.ElapsedMilliseconds / 1000f % SpinnerBoxSpeed * 360f / SpinnerBoxSpeed;
             spinnerBox.Rotation = r;
             Image.Rotation = r;
-            cs.Position = new(Host.Input.TotalMouseDelta, new());
+            cs.Position = new(Host.Input.Mouse.Position, new());
         };
 
         Host.FocusGained += () =>
         {
-            Host.FramePerSecond = 720;
+            Host.FramePerSecond = 480;
         };
         Host.FocusLost += () =>
         {
             Host.FramePerSecond = 50;
         };
 
-        Host.UpdatePerSecond = 10000;
+        Host.UpdatePerSecond = 1000;
 
         FPSPrinter();
 
-        //PerTest(200000,Host.Root);
+        //PerTest(20000,Host.Root);
 
         Host.ShowCursor = false;
-        Host.ParallelRender = false;
+        Host.ParallelRender = true;
         Host.EnableMouseRelative = true;
 
         var input = new UIInput(Host.Resource)
@@ -150,11 +152,12 @@ public static unsafe class SimpleGame
             Parent = Host.Root,
             Z = 100,
             FontId = "Font",
-            CursorColor=new(1,1,1,0.5f),
-            FontScale=1f
+            CursorColor = new(1, 1, 1, 0.5f),
+            FontScale = 1f,
+            Text = "Test",
         };
-        input.Text = "11451419198101145141919810";
-        input.InputPosition=new(3,input.Text.Length-5);
+
+        VisualTouch();
     }
 
     static void FPSPrinter()
@@ -202,5 +205,48 @@ public static unsafe class SimpleGame
                 visible = true,
             };
         }
+    }
+
+    static void VisualTouch()
+    {
+        var TouchC = new UIBox()
+        {
+            Name = "TouchC",
+            Size = new(new(), new(1)),
+            Parent = Host.Root,
+            Z = 1000,
+            color = new(0, 0, 0, 0),
+            CanTouch = false,
+        };
+
+        Host.Input.FingerDown += (a) =>
+        {
+            _ = new UICircle()
+            {
+                Name = $"{a.Id}",
+                Position = new(a.Finger.Position, new()),
+                Size = new(new(25, 25), new()),
+                Anchor = new(0.5f),
+                Parent = TouchC,
+                Z = a.Id,
+                color = new(1, 1, 1, 0.5f),
+            };
+        };
+        Host.Input.FingerMove += (a) =>
+        {
+            var tg = TouchC.FindChildren($"{a.Id}");
+            if (tg.Count > 0 && tg[0] is UICircle b)
+            {
+                b.Position = new(a.Finger.Position, new());
+            }
+        };
+        Host.Input.FingerUp += (a) =>
+        {
+            var tg = TouchC.FindChildren($"{a.Id}");
+            if (tg.Count > 0 && tg[0] is UICircle b)
+            {
+                b.Dispose();
+            }
+        };
     }
 }
